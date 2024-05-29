d2eec48c7a52eace5d4ca3629fd57147881c0cb1
VasliddinAli <vasliddinali@gmail.com>
2024-05-29 15:47:40 +0500

    ...

--- src/views/ShuffleConverter.vue
+++ src/views/ShuffleConverter.vue
@@ -20,6 +20,41 @@
                     @change="selectedSectionChange(shuffledArrays.Section3)" class="sectionInput" />
                 <label for="Section3" class="sectionLabel">3</label>
             </div>
+            <div>
+                <input id="Section4" type="radio" value="Section4" v-model="selectedSection"
+                    @change="selectedSectionChange(shuffledArrays.Section4)" class="sectionInput" />
+                <label for="Section4" class="sectionLabel">4</label>
+            </div>
+            <div>
+                <input id="Section5" type="radio" value="Section5" v-model="selectedSection"
+                    @change="selectedSectionChange(shuffledArrays.Section5)" class="sectionInput" />
+                <label for="Section5" class="sectionLabel">5</label>
+            </div>
+            <div>
+                <input id="Section6" type="radio" value="Section6" v-model="selectedSection"
+                    @change="selectedSectionChange(shuffledArrays.Section6)" class="sectionInput" />
+                <label for="Section6" class="sectionLabel">6</label>
+            </div>
+            <div>
+                <input id="Section7" type="radio" value="Section7" v-model="selectedSection"
+                    @change="selectedSectionChange(shuffledArrays.Section7)" class="sectionInput" />
+                <label for="Section7" class="sectionLabel">7</label>
+            </div>
+            <div>
+                <input id="Section8" type="radio" value="Section8" v-model="selectedSection"
+                    @change="selectedSectionChange(shuffledArrays.Section8)" class="sectionInput" />
+                <label for="Section8" class="sectionLabel">8</label>
+            </div>
+            <div>
+                <input id="Section9" type="radio" value="Section9" v-model="selectedSection"
+                    @change="selectedSectionChange(shuffledArrays.Section9)" class="sectionInput" />
+                <label for="Section9" class="sectionLabel">9</label>
+            </div>
+            <div>
+                <input id="Section10" type="radio" value="Section10" v-model="selectedSection"
+                    @change="selectedSectionChange(shuffledArrays.Section10)" class="sectionInput" />
+                <label for="Section10" class="sectionLabel">10</label>
+            </div>
         </form>
         <div class="head" v-if="arrayData != null">
             <h4 v-for="item in arrayData" :key="item" class="harf">{{ item }}</h4>
@@ -70,7 +105,14 @@ export default {
             shuffledArrays: {
                 Section1: ["M", "Y", "H", "T", "S", "W", "K", "D", "O", "G", "Q", "J", "F", "Z", "C", "U", "N", "B", "P", "A", "V", "I", "R", "X", "L", "E"],
                 Section2: ["C", "H", "I", "L", "N", "B", "K", "J", "U", "Z", "O", "X", "Q", "Y", "R", "D", "V", "E", "A", "S", "F", "G", "M", "P", "T", "W"],
-                Section3: ["G", "M", "P", "R", "N", "W", "Q", "C", "L", "F", "A", "U", "E", "Z", "K", "J", "T", "B", "H", "S", "O", "X", "Y", "I", "D", "V"]
+                Section3: ["G", "M", "P", "R", "N", "W", "Q", "C", "L", "F", "A", "U", "E", "Z", "K", "J", "T", "B", "H", "S", "O", "X", "Y", "I", "D", "V"],
+                Section4: ["H", "B", "R", "Q", "K", "D", "M", "P", "L", "E", "I", "O", "A", "G", "F", "N", "C", "T", "V", "Y", "X", "U", "Z", "W", "J", "S"],
+                Section5: ["S", "M", "P", "W", "D", "L", "R", "E", "G", "N", "A", "C", "F", "B", "T", "V", "Y", "U", "Q", "O", "X", "J", "H", "K", "Z", "I"],
+                Section6: ["J", "T", "H", "N", "X", "I", "D", "A", "M", "B", "P", "W", "K", "R", "S", "Q", "O", "C", "F", "V", "U", "L", "Z", "E", "G", "Y"],
+                Section7: ["L", "H", "P", "G", "Q", "F", "V", "A", "O", "S", "M", "K", "N", "C", "U", "E", "X", "T", "I", "W", "D", "R", "Y", "B", "J", "Z"],
+                Section8: ["W", "R", "B", "S", "F", "P", "T", "H", "J", "E", "Y", "G", "N", "A", "K", "Z", "C", "I", "V", "M", "D", "O", "U", "Q", "L", "X"],
+                Section9: ["U", "N", "W", "G", "V", "A", "B", "S", "J", "H", "D", "O", "R", "E", "Y", "T", "L", "P", "K", "I", "X", "Q", "Z", "M", "C", "F"],
+                Section10: ["O", "S", "I", "C", "U", "E", "L", "K", "P", "W", "N", "T", "Q", "X", "F", "D", "H", "J", "V", "G", "A", "B", "R", "Y", "M", "Z"],
             },
             selectedSection: '',
             inputText: '',
@@ -82,7 +124,6 @@ export default {
     methods: {
         selectedSectionChange(val) {
             this.arrayData = val
-            console.log(val);
         },
         convertText() {
             if (!this.selectedSection) {
@@ -113,13 +154,14 @@ export default {
 </script>
 
 <style scoped>
-h5{
+h5 {
     padding: 0;
     margin: 0;
     padding-bottom: 20px;
     text-align: center;
     font-size: 20px;
 }
+
 .head {
     display: flex;
     align-items: center;
